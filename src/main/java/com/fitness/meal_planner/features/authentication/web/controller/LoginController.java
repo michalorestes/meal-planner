6fde6d46ec0fdf757d8e8c03c7d7d8f0374ee18c
Michal Dolinski <michalorestes@gmail.com>
2025-08-31 18:14:35 +0100

    wip

--- src/main/java/com/fitness/meal_planner/features/authentication/web/controller/LoginController.java
+++ src/main/java/com/fitness/meal_planner/features/authentication/web/controller/LoginController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SignUpController {
-    @GetMapping("/signup")
-    public String signUp() {
-        return "authentication/signupForm";
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "authentication/loginForm";
     }
+    
 }
